31b9085b3423085e76152d6d67ebeff3b8984d68
Tylor Sherman <tylor@denimandsteel.com>
2013-02-07 14:37:44 -0800

    Add types attribute

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,10 +1,18 @@
 class Asset < ActiveRecord::Base
+  TYPES = [
+    ['Character', 'character'],
+    ['Prop', 'prop'],
+    ['Background', 'background'],
+    ['Effect', 'effect'],
+    ['Audio', 'audio'],
+  ]
+
 	has_paper_trail
   mount_uploader :asset, AssetUploader
 
   has_many :reel_assets, :dependent => :delete_all
 
-  acts_as_taggable_on :keywords, :techniques
+  acts_as_taggable_on :keywords
 
   attr_accessible :description,
                   :title,
@@ -13,8 +21,11 @@ class Asset < ActiveRecord::Base
                   :scene,
                   :shot,
                   :category,
+                  :type,
                   :keyword_list,
                   :asset
+
+  #validates_inclusion_of :type, :in => TYPES
 
  before_create :default_name
   
