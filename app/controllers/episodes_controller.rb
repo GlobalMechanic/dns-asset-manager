2f46b3d2eba678bfd76694cf8389515209bf589c
Tylor Sherman <tylor@denimandsteel.com>
2013-02-20 11:44:25 -0800

    Auth for scenes and episodes

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,5 +1,4 @@
 class EpisodesController < InheritedResources::Base
   respond_to :html, :json
-  #before_filter :authenticate_user!
-  #load_and_authorize_resource
+  before_filter :authenticate_user!
 end
