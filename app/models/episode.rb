67eb6884a6505bfde9f185ac2289955fa3c875e6
Tylor Sherman <tylor@denimandsteel.com>
2013-02-27 14:05:40 -0800

    Make sure episode number and season is a number

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,4 +2,6 @@ class Episode < ActiveRecord::Base
   #has_many :assets, :through => :reel_assets
   attr_accessible :number, :title, :season
   has_many :scenes
+  validates :season, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :number, :numericality => { :only_integer => true, :greater_than => 0 }
 end
