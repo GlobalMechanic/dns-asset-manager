fa5577ab523d24f814e6f1e8fe18f1aac63948b8
Tylor Sherman <tylor@denimandsteel.com>
2013-02-28 14:01:35 -0800

    Order episodes correctly

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -3,4 +3,9 @@ class EpisodesController < InheritedResources::Base
   load_and_authorize_resource
   before_filter :authenticate_user!
   before_filter :add_reels
+
+  protected
+  def collection
+    @episodes = end_of_association_chain.order('number')
+  end
 end
