adb84c3303bcf0d316801d94e885616d5d9cdd5b
Tylor Sherman <tylor@denimandsteel.com>
2013-02-27 21:00:29 -0800

    Have to load current_user after we authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,9 @@ class ApplicationController < ActionController::Base
     @current_reel = current_user && current_user.current_reel_slug? && Reel.exists?(current_user.current_reel_slug) ? Reel.find(current_user.current_reel_slug) : nil
     @reels_created = Reel.where("title <> ''").order('created_at DESC').limit(5)
     @reels_updated = Reel.where("title <> ''").order('updated_at DESC').limit(5)
+  end
 
+  def add_reels
     if @current_reel
       @reel = @current_reel
     else
@@ -21,4 +23,5 @@ class ApplicationController < ActionController::Base
       @current_reel = @reel
     end
   end
+
 end
--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -2,4 +2,5 @@ class EpisodesController < InheritedResources::Base
   respond_to :html, :json
   load_and_authorize_resource
   before_filter :authenticate_user!
+  before_filter :add_reels
 end
