fdbd27f63db652a2b968408d1ce4969ad806e062
Tylor Sherman <tylor@denimandsteel.com>
2013-03-07 23:31:06 -0800

    No 1

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -23,7 +23,7 @@ class AssetUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}/1"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def filename
