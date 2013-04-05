0694fa6257c860030b3163be739411c4a67f49b7
Tylor Sherman <tylor@denimandsteel.com>
2013-04-05 15:17:54 -0700

    mp4 not mp3

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -12,7 +12,7 @@ class AssetUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   IMAGE_EXTENSIONS = %w(jpg jpeg gif png)
-  MOVIE_EXTENSIONS = %w(mov mp3)
+  MOVIE_EXTENSIONS = %w(mov mp4)
   FLASH_EXTENSIONS = %w(fla flv swf)
   AUDIO_EXTENSIONS = %w(mp3 wav)
   
