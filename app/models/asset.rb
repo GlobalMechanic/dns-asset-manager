add9b11de1643d439ebc0ff1382bc1cf1294165b
Tylor Sherman <tylor@denimandsteel.com>
2013-05-01 14:03:34 -0700

    Add Posing step

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -14,6 +14,7 @@ class Asset < ActiveRecord::Base
   STATUS = [
     ['Setup', 'setup'],
     ['Layout', 'layout'],
+    ['Posing', 'posing'],
     ['Animation', 'animation'],
     ['Approved', 'approved'],
   ]
