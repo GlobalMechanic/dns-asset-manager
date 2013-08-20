64f9674039bca5d0296c6ce8ed69cff102e696e8
Tylor Sherman <tylor@denimandsteel.com>
2013-08-19 22:17:00 -0700

    Add from address

--- app/mailers/download_mailer.rb
+++ app/mailers/download_mailer.rb
@@ -1,6 +1,7 @@
 class DownloadMailer < ActionMailer::Base
   def download_ready(email, episode, upload_name)
     mail(
+      from: "studio@globalmechanic.com",
       to: email,
       subject: "Your episode is ready for #{episode.number} #{episode.title}",
       body: "Click to download: https://asset-manager-testing.s3.amazonaws.com/#{upload_name}",
